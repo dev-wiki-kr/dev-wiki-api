d5592bcac1c15f2c078261c3f1ddcffb799d356b
sming156@interpark.com <sming156@interpark.com>
2024-05-26 20:48:47 +0900

    환경변수 주입 방법 변경

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-echo "DB_HOST=${{ secrets.DB_HOST }}" >> .env.production
-echo "DB_NAME=${{ secrets.DB_NAME }}" >> .env.production
-echo "DB_PASSWORD=${{ secrets.DB_PASSWORD }}" >> .env.production
-echo "DB_PORT=${{ secrets.DB_PORT }}" >> .env.production
-echo "DB_SYNCHRONIZE=${{ secrets.DB_SYNCHRONIZE }}" >> .env.production
-echo "DB_USERNAME=${{ secrets.DB_USERNAME }}" >> .env.production
-echo "POSTGRES_DB=${{ secrets.POSTGRES_DB }}" >> .env.production
-echo "POSTGRES_PASSWORD=${{ secrets.POSTGRES_PASSWORD }}" >> .env.production
-echo "POSTGRES_USER=${{ secrets.POSTGRES_USER }}" >> .env.production
+echo $DB_HOST >> .env.production
+echo $DB_NAME >> .env.production
+echo $DB_PASSWORD >> .env.production
+echo $DB_PORT >> .env.production
+echo $DB_SYNCHRONIZE >> .env.production
+echo $DB_USERNAME >> .env.production
+echo $POSTGRES_DB >> .env.production
+echo $POSTGRES_PASSWORD >> .env.production
+echo $POSTGRES_USER >> .env.production
 
-echo "DB_HOST=${{ secrets.DB_HOST }}" >> .env.development
-echo "DB_NAME=${{ secrets.DB_NAME }}" >> .env.development
-echo "DB_PASSWORD=${{ secrets.DB_PASSWORD }}" >> .env.development
-echo "DB_PORT=${{ secrets.DB_PORT }}" >> .env.development
-echo "DB_SYNCHRONIZE=${{ secrets.DB_SYNCHRONIZE }}" >> .env.development
-echo "DB_USERNAME=${{ secrets.DB_USERNAME }}" >> .env.development
-echo "POSTGRES_DB=${{ secrets.POSTGRES_DB }}" >> .env.development
-echo "POSTGRES_PASSWORD=${{ secrets.POSTGRES_PASSWORD }}" >> .env.development
-echo "POSTGRES_USER=${{ secrets.POSTGRES_USER }}" >> .env.development
+echo $DB_HOST >> .env.development
+echo $DB_NAME >> .env.development
+echo $DB_PASSWORD >> .env.development
+echo $DB_PORT >> .env.development
+echo $DB_SYNCHRONIZE >> .env.development
+echo $DB_USERNAM >> .env.development
+echo $POSTGRES_DB >> .env.development
+echo $POSTGRES_PASSWORD >> .env.development
+echo $POSTGRES_USER >> .env.development
 
 docker compose down
 
