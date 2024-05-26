ff7105964936a9ffd6080c70615902f1bde78fa4
sming156@interpark.com <sming156@interpark.com>
2024-05-26 15:56:16 +0900

    letsencrypt staging 설정 변경

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(devwiki.co.kr www.devwiki.co.kr)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="hwangstar156@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
